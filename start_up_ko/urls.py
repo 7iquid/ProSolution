ae582de78b8358eb9bfb81dc7c3c70c346206a64
tavs <taviefalcon@yahoo.com>
2021-10-16 01:56:41 +0800

    flask to django hehehe

--- start_up_ko/urls.py
+++ start_up_ko/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path("",views.index, name = "index"),
-path("v1/",views.v1, name = "view 1"),
+    path("", views.index, name="index"),
+    # path("v1/",views.v1, name = "view 1"),
+    path("v1/<int:id>", views.v1, name="view 1"),
+
 ]
--- start_up_ko/views.py
+++ start_up_ko/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 # Create your views here.
 
+
 def index(response):
-	return HttpResponse("<h1> bagong </h1>")
+    return HttpResponse("<h1> bagong </h1>")
 
 
-def v1(response):
-	return HttpResponse("<h1> bagong </h1>")
+def v1(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return HttpResponse("<h1> %s </h1>" % ls.name)
+
 
 def home(response):
-	pass
+    pass
